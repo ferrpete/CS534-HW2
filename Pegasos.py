b6d5850d1e92e79af925d95b138f97e59b9856fb
ferrpete <32526300+ferrpete@users.noreply.github.com>
2017-10-28 15:41:20 -0700

    Debugged Pegasos, Recalculated Objective Function, Added Plots
    Hey all-
    
    I found a few bugs in the Pegasos algorithm file.  I redid the plots for the error rates and objective function.  They now match very closely to the SVM results.  All of the fixes have been reported in the Overleaf file, too.
    
    -Peter

--- Pegasos.py
+++ Pegasos.py
@@ -1,13 +1,14 @@
 import numpy as np
 import time
 from Predictor import Predictor
+import matplotlib.pyplot as plt
 #from sklearn import svm
 import DataProcessor
 
 ## Utilize sklearn's SVM program to classify individuals earning less
 ## or more than 50K/year.
 
-def DevError(weightVector, data, target):
+def DevError(weightVector, bias, data, target):
  
         i = 0
         positiveResults = 0
@@ -15,8 +16,7 @@ def DevError(weightVector, data, target):
 
         for row in data:
             #row = row
-            
-            if target[i] * np.dot(row, weightVector) <= 0:
+            if target[i] * (np.dot(row, weightVector) + bias) <= 0:
                 # <= 50
                errors += 1
             
@@ -25,54 +25,80 @@ def DevError(weightVector, data, target):
         error_rate = errors / len(data)        
         return error_rate
 
-def Pegasos(data, target, devData, devTarget, cParam = 1, _kernel='linear', _degree=1, _coef0=0):
+def Pegasos(data, target, devData, devTarget):
 
-    nCount = len(data[0])
-    weightVector = np.zeros(nCount)
+    nCount = len(data)
+    weightVector = np.zeros(len(train_data[0]))
+    bias = 0
     cParam = 1
     lambdaVar = 1 / (nCount * cParam)
     currentTrainingCount = 1
-    epochCount = 0
-    totalEpoch = 6
+    epochCount = 1
+    totalEpoch = 250
+##    supportVectors = 0
+    startTime = time.time()
+    objective = []
+    train_Error = []
+    dev_Error = []
+    epoch = []
 
     while epochCount <= totalEpoch:
 
           errors = 0
-
-          startTime = time.time()
           for i in range(0, nCount):
        
-              learningRate = 1 / (lambdaVar / currentTrainingCount)
+              learningRate = 1 / (lambdaVar * currentTrainingCount)
 
-              if (target[i] * np.dot(data[i], weightVector)) < 1:
+              if target[i] * (np.dot(data[i], weightVector) + bias) < 1:
 
-                  weightVector = weightVector - learningRate * (lambdaVar * weightVector - weightVector * data[i])
+                  weightVector = weightVector - learningRate * (lambdaVar * weightVector - target[i] * data[i])
+                  bias = bias - learningRate * (lambdaVar * bias - target[i])
+##                  supportVectors += 1
 
               else:
                   weightVector = weightVector - learningRate * lambdaVar * weightVector
-
+                  bias = bias - learningRate * lambdaVar * bias
+              
               currentTrainingCount += 1
 
           endTime = time.time()       
           epochCount += 1
 
-          trainError = DevError(weightVector, data, target)
-          devError = DevError(weightVector, devData, devTarget)
+          trainError = DevError(weightVector, bias, data, target)
+          devError = DevError(weightVector, bias, devData, devTarget)
           
-          print("The dev error rate is {:.2%} for epoch = %d", devError, epochCount)
+          print("The dev error rate is {:.2%} for epoch = {:1d}".format(devError, epochCount))
 
-          print("The Training error rate for epoch %d is %f ", epochCount, trainError )
+          print("The Training error rate is {:.2%} for epoch = {:1d} ".format(trainError, epochCount))
       
-          print("The Pegasos ran for %s seconds" % (endTime - startTime))
-          #print("The number of support vectors are: ", str(len(clf.support_vectors_)))
+          print("The Pegasos ran for {:.2f} seconds".format((endTime - startTime)))
 
-    return weightVector
+##          print(str(supportVectors))
+
+          objective.append(objective_function(weightVector, bias, data, target, cParam))
+          train_Error.append(100 * trainError)
+          dev_Error.append(100 * devError)
+          epoch.append(epochCount - 1)
+    
+    return weightVector, objective, train_Error, dev_Error, epoch
+
+def objective_function(model, bias, train_data, target, cParam = 1):
+    totalSlack = 0
+    for j in range(len(train_data)):
+        slack = 1 - target[j] * (model.dot(train_data[j]) + bias)
+        if slack > 0:
+            totalSlack += slack
+        
+    objective = 0.5 * (model.dot(model.T) + bias * bias) + cParam * totalSlack
+
+    print("The minimized objective function value is {:.2f}".format(objective))
+    return objective
 
 def test(dataSet, data, model, bias, cParam = 1):
     errors = sum(y * (model.dot(vecx) + bias) <= 0 for vecx, y in data)
     error_rate = errors / len(data)
 
-    print("The", dataSet, "error rate is {:.2%} for C = {:.2f}".format(error_rate[0], cParam))
+    print("The", dataSet, "error rate is {:.2%} for C = {:.2f%}".format(error_rate[0], cParam))
 
 
 if __name__ == "__main__":
@@ -82,15 +108,17 @@ if __name__ == "__main__":
     train_data, train_target = DataProcessor.map_data(train_file, feature2index)
     dev_data, dev_target = DataProcessor.map_data(dev_file, feature2index)
 
-    _kernel = int(input("Kernel [1: linear | 2: quadratic] > "))
-    kernel = 'linear'
-    degree = 1
-    coef0 = 0
-    if _kernel == 2:
-        kernel = 'poly'
-        degree = 2
-        coef0 = 1
+    weightVector, objective, trainError, devError, epoch = Pegasos(train_data, train_target, dev_data, dev_target)
 
-    while True:
+    plt.plot(epoch, trainError, 'r-', epoch, devError, 'k-')
+    plt.legend(('Training Set', 'Dev Set'))
+    plt.axis([0, 250, 15, 25])
+    plt.xlabel('Epoch')
+    plt.ylabel('Error Rate, %')
+    plt.show()
 
-          w = Pegasos(train_data, train_target, dev_data, dev_target)
+    plt.plot(epoch, objective, 'k-')
+    plt.axis([0, 250, 1800, 6000])
+    plt.xlabel('Epoch')
+    plt.ylabel('Objective Function')
+    plt.show()
